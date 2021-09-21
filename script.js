4f14c7675791d441ef6b51ea7df241514618307c
Gabriel A <gabrielaug4@gmail.com>
2021-09-21 19:29:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let n1 = document.getElementById('n1');
+let n2 = document.getElementById('n2');
+
+const plus = document.getElementById('plus');
+const sub = document.getElementById('sub');
+const divis = document.getElementById('divis');
+const mult = document.getElementById('mult');
+
+function plus() {
+
+}
+
+function sub() {
+
+}
+
+function divis() {
+
+}
+
+function mult() {
+    
+}
+
+
+plus.addEventListener('click', plus);
+sub.addEventListener('click',sub);
+divis.addEventListener('click',divis);
+mult.addEventListener('click', mult);
